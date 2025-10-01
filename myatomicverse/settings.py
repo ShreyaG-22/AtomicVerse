ea0c3bb1cbc2589cc5eb22c736d1fa51b2c44b31
ShreyaG-22 <shreyagade22@gmail.com>
2025-10-01 22:18:54 +0530

    Complete deployment setup with Procfile and production settings

--- myatomicverse/settings.py
+++ myatomicverse/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!01x)i=%2j*sgp1=txa_&s1nfbkhq80_rib^yj*p05g&275ee#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
